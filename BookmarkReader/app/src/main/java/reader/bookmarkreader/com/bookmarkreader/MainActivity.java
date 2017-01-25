2bd74f54d9fa7ae7ca930112d327aac02eb9b4b2
DavutOzcan <davutozcan87@gmail.com>
2017-01-25 22:28:40 +0300

    Feature: Navigation draver has been add.

--- BookmarkReader/app/src/main/java/reader/bookmarkreader/com/bookmarkreader/MainActivity.java
+++ BookmarkReader/app/src/main/java/reader/bookmarkreader/com/bookmarkreader/MainActivity.java
@@ -1,14 +1,32 @@
 package reader.bookmarkreader.com.bookmarkreader;
 
 import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends Activity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        toolbar.setTitle("Bookmark Reader");
+        this.toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+//        toggle.syncState();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        toggle.syncState();
+        super.onPostCreate(savedInstanceState);
     }
 }
