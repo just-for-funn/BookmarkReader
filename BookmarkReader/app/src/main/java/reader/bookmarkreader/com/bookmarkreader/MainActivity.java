bb2bfbe08cfae22c806d13f4e43803443295ef40
DavutOzcan <davutozcan87@gmail.com>
2017-02-17 01:10:29 +0300

    Fragment drafts has been added.

--- BookmarkReader/app/src/main/java/reader/bookmarkreader/com/bookmarkreader/MainActivity.java
+++ BookmarkReader/app/src/main/java/reader/bookmarkreader/com/bookmarkreader/MainActivity.java
@@ -1,6 +1,8 @@
 package reader.bookmarkreader.com.bookmarkreader;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +10,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
@@ -21,6 +23,13 @@ public class MainActivity extends Activity {
         toolbar = (Toolbar)findViewById(R.id.toolBar);
         toolbar.setTitle("Bookmark Reader");
         this.toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+
+
+        WebSiteListFragment fragment = new WebSiteListFragment();
+        FragmentManager manager =  getSupportFragmentManager();
+        manager.beginTransaction()
+                .add(R.id.main_view_content , fragment)
+                .commit();
     }
 
     @Override
---
+++ BookmarkReader/app/src/main/java/reader/bookmarkreader/com/bookmarkreader/WebSiteListFragment.java
@@ -0,0 +1,33 @@
+package reader.bookmarkreader.com.bookmarkreader;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by davut on 15.02.2017.
+ */
+
+public class WebSiteListFragment extends Fragment
+{
+
+    TextView textView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View w =  inflater.inflate(R.layout.website_list_fragment , container , false);
+        this.textView = (TextView) w.findViewById(R.id.txt1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().add(R.id.main_view_content , new DetailFragment()).commit();
+            }
+        });
+        return w;
+    }
+}
